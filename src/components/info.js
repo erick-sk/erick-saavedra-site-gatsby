7b1e7e42100f9b35fb1390d6f6ea4c37b71f7aa0
Erick Saavedra <esvdrah@gmail.com>
2020-07-18 23:04:43 -0500

    end project v1

--- src/components/info.js
+++ src/components/info.js
@@ -3,9 +3,9 @@ import React from "react"
 import {
   FaTwitter as Twitter,
   FaGithub as Github,
-  FaYoutube as Youtube,
   FaInstagram as Insta,
   FaEnvelope as Mail,
+  FaLinkedin as Linkedin,
 } from "react-icons/fa"
 
 export default function info() {
@@ -13,35 +13,34 @@ export default function info() {
     <div className="container">
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
-        <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
+        <span style={{ color: "white" }}>Hello, I'm</span> <br />
+        Erick Saavedra.
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
-        A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
-        Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
+        A Software Development student, IoT Geek and Cat Lover.
+        <br /> "When virtue has slept, she will get up more refreshed."
+        <br />- Friedrich Nietzsche
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
-        <a className="mr-5 icon" href="https://twitter.com/Mr404Found">
+        <a className="mr-5 icon" href="https://twitter.com/ericksk_">
           <Twitter />
         </a>
-        <a className="mr-5 icon" href="https://instagram.com/sumanth.zero7">
+        <a className="mr-5 icon" href="https://instagram.com/ericksk_">
           <Insta />
         </a>
-        <a className="mr-5 icon" href="https://github.com/Mr404Found">
+        <a className="mr-5 icon" href="https://github.com/erick-sk">
           <Github />
+        </a>
+        <a className="mr-5 icon" href="mailto://esvdrah@gmail.com">
+          <Mail />
         </a>
         <a
           className="mr-5 icon"
-          href="https://www.youtube.com/channel/UCw0FDIL-HpcfzEZrjcQwhMQ"
+          href="https://www.linkedin.com/in/erick-saavedra-dev"
         >
-          <Youtube />
-        </a>
-        <a className="mr-5 icon" href="mailto://contactmr404here@gmail.com">
-          <Mail />
+          <Linkedin />
         </a>
       </div>
     </div>
